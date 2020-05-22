31474a1e633efe8ec5cd07ceea8466242d788636
GabrielKishida <gabriel.kishida@usp.br>
2020-05-22 04:24:58 -0300

    :goal_net: Adding exception catchers

--- src/MapReader.java
+++ src/MapReader.java
@@ -21,7 +21,11 @@ public class MapReader {
 	ArrayList<String> rawMap = new ArrayList<String>();	//	Mapa cru, lido do arquivo txt 
 	
 	MapReader(String fileName) {
-		readFile(fileName);	
+		readFile(fileName);
+		if(!validMap()) { //Caso o mapa seja invalido, imprime mensagem de erro e termina a execucao
+			System.out.println("The map format is not valid.");
+			System.exit(0);
+		}
 	}
 	
 	/*
@@ -44,11 +48,11 @@ public class MapReader {
 	}
 	
 	/*
-	 * @brief Verifica a validade do mapa lido
+	 * @brief Verifica se o mapa lido eh retangular
 	 * 
-	 * @return	Se mapa eh valido
+	 * @return	Se o mapa eh retangular
 	 */
-	boolean validMap() throws Exception {
+	boolean rectangularMap() throws Exception {
 		int widthReference = rawMap.get(0).length();
 		for(String line: rawMap) {
 			if(line.length() != widthReference) 
@@ -57,14 +61,61 @@ public class MapReader {
 		return true;
 	}
 	
-	//GETTERS
+	/*
+	 * @brief Verifica se o mapa tem 4 paredes em seus 4 vertices
+	 * 
+	 * @return	Se o mapa tem paredes nos 4 vertices ou nao
+	 */
+	boolean hasCorners() throws Exception {
+		if(rawMap.get(0).charAt(0) == '#' &&
+			rawMap.get(0).charAt(getWidth()-1) == '#' &&
+			rawMap.get(getHeight()-1).charAt(0) == '#' && 
+			rawMap.get(getHeight()-1).charAt(getWidth()-1) == '#') 
+			return true;	
+		else {
+			throw new WrongMapFormatException("The map must have '#' on 4 corners");	//joga exception, pois eh uma das definicoes do mapa para nao ter erro
+		}
+	}
 	
+	/*
+	 * @brief Verifica se o mapa tem apenas um pacman
+	 * 
+	 * @return	Se o mapa tem apenas um pacman ou nao
+	 */
+	boolean hasOnePacman() throws Exception {
+		int numberOfPacmans = 0;
+		for(String line : rawMap) {
+			for(int i = 0; i < line.length(); i++) {
+				if(line.charAt(i) == 'C') numberOfPacmans++;
+			}
+		}
+		if (numberOfPacmans == 1)
+			return true;
+		else
+			throw new WrongMapFormatException("The map must have only ONE 'C'");	//joga exception, pois eh uma das definicoes do mapa para nao ter erro
+	}
+	
+	/*
+	 * @brief Verifica se o mapa eh valido, de acordo com as condicoes instruidas
+	 * 
+	 * @return	Se o mapa eh valido ou nao
+	 */
+	boolean validMap() {
+		try {
+			if(rectangularMap() && hasCorners() && hasOnePacman()) //O mapa deve ser retangular, ter parede em seus 4 vertices, e ter apenas um pacman
+				return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} return false;
+	}
+	
+	//GETTERS
 	int getHeight() { return rawMap.size(); }
 	ArrayList<String> getInput() { return rawMap; }
 	
 	int getWidth() {
 		try {
-			if(validMap()) return rawMap.get(0).length();
+			if(rectangularMap()) return rawMap.get(0).length();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
